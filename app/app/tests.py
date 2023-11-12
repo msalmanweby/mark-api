f86a1f5a59fa06ec62092a274bcdcd65eaaa17c2
Muhammad Salman <msalman2339@gmail.com>
2023-11-13 04:14:54 +0500

    Setup Database Postgresql

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,8 +1,9 @@
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTest(SimpleTestCase):
-    def test_add_numbers(self):
+    def test_add_numbers(self ):
         res = calc.add(6,5)
         self.assertEqual(res, 11)
         
