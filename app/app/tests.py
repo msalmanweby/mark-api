ecdcaf656d108c9a71c2397b2943f35492608f1b
Muhammad Salman <msalman2339@gmail.com>
2023-11-14 05:30:14 +0500

    Adding Core App

--- app/app/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import SimpleTestCase
-from app import calc
-
-
-class CalcTest(SimpleTestCase):
-    def test_add_numbers(self):
-        res = calc.add(6, 5)
-        self.assertEqual(res, 11)
